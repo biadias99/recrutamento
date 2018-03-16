8b66c96a9c5decad2b7a8e5e1840eead82af5d67
biadias99 <biadiasbarbosa@hotmail.com>
2018-03-16 16:34:46 -0300

    estudo vue.js

---
+++ projetos_teste_vuejs/02_login/js/index.js
@@ -0,0 +1,12 @@
+  var app = new Vue({
+           el : "#app",
+           data : {
+               usuario : {
+                   login : "usuario123",
+                   tipo : "admin"
+               },
+            feedback : {
+                   enviado : false
+               }
+           }
+  })
